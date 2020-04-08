cb09823a3f2f02124fe5d4417dffe62f669379a6
maitreebain <maitreebain@gmail.com>
2020-04-07 21:10:59 -0400

    added basic ui working on createUserVC

---
+++ KVO-Lab/KVO-Lab/CreateUserController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO-Lab
+//
+//  Created by Maitree Bain on 4/7/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class CreateUserController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ KVO-Lab/KVO-Lab/DepositWithdrawController.swift
@@ -0,0 +1,30 @@
+//
+//  DepositWithdrawController.swift
+//  KVO-Lab
+//
+//  Created by Maitree Bain on 4/7/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class DepositWithdrawController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
